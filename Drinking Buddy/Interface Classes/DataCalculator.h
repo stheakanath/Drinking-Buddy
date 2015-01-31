1c8f163031bf04c1a9545f5721b51a983d141c13
Sony Theakanath <stheakanath@gmail.com>
2015-01-30 22:19:36 -0800

    Added Data class

---
+++ Drinking Buddy/Interface Classes/DataCalculator.h
@@ -0,0 +1,15 @@
+//
+//  DataCalculator.h
+//  Drinking Buddy
+//
+//  Created by Sony Theakanath on 1/30/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataCalculator : NSObject
+
++ (double) calculateAlcohol:(NSDictionary*)dict;
+
+@end
---
+++ Drinking Buddy/Interface Classes/DataCalculator.m
@@ -0,0 +1,18 @@
+//
+//  DataCalculator.m
+//  Drinking Buddy
+//
+//  Created by Sony Theakanath on 1/30/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import "DataCalculator.h"
+
+@implementation DataCalculator
+
++ (double) calculateAlcohol:(NSDictionary*)dict {
+    // example: double x = dict[@"element_name"];
+    return 0.0;
+}
+
+@end
