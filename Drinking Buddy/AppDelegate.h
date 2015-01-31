bfb04bc71f0b681f9e41ae38f0ddb42f1a5ca3cb
Sony Theakanath <stheakanath@gmail.com>
2015-01-30 21:47:22 -0800

    Initial Commit

---
+++ Drinking Buddy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drinking Buddy
+//
+//  Created by Sony Theakanath on 1/30/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Drinking Buddy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Drinking Buddy
+//
+//  Created by Sony Theakanath on 1/30/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
